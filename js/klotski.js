750b9df56fcce7565c866cdd130f49c7706592e0
Gustavo Brian <gustavo-dm2010@hotmail.com>
2022-09-29 11:34:30 -0300

    alteracoes nas funcoes

--- js/klotski.js
+++ js/klotski.js
@@ -6,24 +6,56 @@ for (let i = 0; i < 9; i++){
 
 let pecas = [
     1,2,3,
-    4,5,6,
-    7,0,8
+    4,0,6,
+    7,5,8
 ];
 
-for (let i = 0; i < casas.length; i++){
-    document.addEventListener('keydown', (event) => {
-        let tecla = event.key;
-        let casa = casas[i];
-        const indice = pecas.indexOf(0);
+let indice = pecas.indexOf(0);
 
-        if (casa.innerHTML == ''){
-            moverDireita(indice, tecla, casas);
-        }
-    })
+document.addEventListener('keydown', (event) => {
+    let tecla = event.key;
+    let casa = casas[indice];
+    
+    
+    if (casa.innerHTML == ''){
+        switch(tecla){
+            case 'ArrowRight':
+                moverDireita();
+            case 'ArrowLeft':
+                moverEsquerda();
 
-    const moverDireita = (indice, tecla, casas) => {
-        if (indice != 2 && indice != 5 && indice != 8 && tecla == 'ArrowRight'){
-            casas[i] = indice+1
         }
+        
+    }
+})
+
+const trocarCasas = (idx, a, b, c) => {
+    if (indice != a && indice != b && indice != c){
+        console.log('indice '+indice);
+        console.log('idx '+idx);
+        casas[idx].classList.add("bloco-vazio");
+        casas[indice].classList.add("bloco");
+        
+        casas[idx].classList.remove("bloco");
+        casas[indice].classList.remove("bloco-vazio");
+
+        casas[indice].innerText = casas[idx].innerText;
+        casas[idx].innerText = '';
+
+        pecas[indice] = parseInt(casas[indice].innerText);
+        pecas[idx] = 0;
+        indice = pecas.indexOf(0);
+        console.log(pecas);
+        console.log('indice '+indice);
+        console.log('idx '+idx);
     }
 }
+
+const moverDireita = () => {
+    return trocarCasas(indice-1, 0, 3, 6);
+}
+
+const moverEsquerda = () => {
+    return trocarCasas(indice+1, 2, 5, 8);
+}
+        
